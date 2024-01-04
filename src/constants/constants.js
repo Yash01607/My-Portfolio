074865598b04dd4e9d343bab8577847de3fe628d
Yash Agrawal <62216513+Yash01607@users.noreply.github.com>
2024-01-04 17:43:22 +0530

    Added Certificates to constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,83 +1,118 @@
 export const projects = [
   {
-    title: 'AgroTech',
+    title: "AgroTech",
     description:
-      'Developed an E-commerce website using MERN Stack for agricultural products. It allows users to securely login and purchase products. Also implemented a live chat feature using Socket.io enabling customers to engage in real-time conversations with admin users.',
-    image: './images/agrotech.png',
-    tags: ['ReactJS', 'ReduxJS', 'NodeJS', 'ExpressJS', 'MongoDB', 'Socket.io'],
-    source: 'https://github.com/Yash01607/Ecommerce_website',
-    visit: 'https://agrotech-yajt.onrender.com/',
+      "Developed an E-commerce website using MERN Stack for agricultural products. It allows users to securely login and purchase products. Also implemented a live chat feature using Socket.io enabling customers to engage in real-time conversations with admin users.",
+    image: "./images/agrotech.png",
+    tags: ["ReactJS", "ReduxJS", "NodeJS", "ExpressJS", "MongoDB", "Socket.io"],
+    source: "https://github.com/Yash01607/Ecommerce_website",
+    visit: "https://agrotech-yajt.onrender.com/",
     id: 1,
   },
   {
-    title: 'CrowdWallet',
+    title: "CrowdWallet",
     description:
-      'Crowd Wallet is designed to assist entrepreneurs in raising crowd funds for their projects. Built using Blockchain Technology to promote transparency in spending of funds collected, this application consists of voting system where all contributors cast vote expressing their approval or disapproval of the spending request.',
-    image: './images/crowdwallet.png',
-    tags: ['NextJS', 'Solidity', 'Web3'],
-    source: 'https://github.com/Yash01607/CrowdCoin',
-    visit: 'https://crowd-coin-yash01607.vercel.app/',
+      "Crowd Wallet is designed to assist entrepreneurs in raising crowd funds for their projects. Built using Blockchain Technology to promote transparency in spending of funds collected, this application consists of voting system where all contributors cast vote expressing their approval or disapproval of the spending request.",
+    image: "./images/crowdwallet.png",
+    tags: ["NextJS", "Solidity", "Web3"],
+    source: "https://github.com/Yash01607/CrowdCoin",
+    visit: "https://crowd-coin-yash01607.vercel.app/",
     id: 2,
   },
   {
-    title: 'Memoreaum',
+    title: "Memoreaum",
     description:
-      'Developed a user-friendly social media application utilizing the MERN stack. It allows users to effortlessly share captivating moments and events from their lives. Users can also like and comment on these shared memories',
-    image: './images/memoreaum.png',
-    tags: ['MongoDB', 'ExpressJS', 'ReactJS', 'NodeJS'],
-    source: 'https://github.com/Yash01607/Memoreaum',
-    visit: 'https://memoreaum.onrender.com/',
+      "Developed a user-friendly social media application utilizing the MERN stack. It allows users to effortlessly share captivating moments and events from their lives. Users can also like and comment on these shared memories",
+    image: "./images/memoreaum.png",
+    tags: ["MongoDB", "ExpressJS", "ReactJS", "NodeJS"],
+    source: "https://github.com/Yash01607/Memoreaum",
+    visit: "https://memoreaum.onrender.com/",
     id: 0,
   },
   {
-    title: 'Expense Tracker',
+    title: "Expense Tracker",
     description:
-      'Developed a userifriendly Expense Tracker Web-App where users can digitally keep track of their monthly expenses.',
-    image: './images/expense-tracker.png',
-    tags: ['ReactJS', 'NodeJS', 'ExpressJS', 'Mongoose'],
-    source: 'https://github.com/Yash01607/Expense-Tracker',
-    visit: 'https://expense-tracker-t01y.onrender.com/',
+      "Developed a userifriendly Expense Tracker Web-App where users can digitally keep track of their monthly expenses.",
+    image: "./images/expense-tracker.png",
+    tags: ["ReactJS", "NodeJS", "ExpressJS", "Mongoose"],
+    source: "https://github.com/Yash01607/Expense-Tracker",
+    visit: "https://expense-tracker-t01y.onrender.com/",
     id: 3,
   },
 ];
 
 export const EducationDetails = [
   {
-    year: 'Present',
-    designation: 'Software Developer',
-    institute: 'Beehyv Software Solutions Pvt. Ltd.',
+    year: "Present",
+    designation: "Software Developer",
+    institute: "Beehyv Software Solutions Pvt. Ltd.",
   },
   {
     year: 2023,
-    designation: 'B.Tech in Petroleum Engineering',
-    institute: 'IIT(ISM) Dhanbad',
+    designation: "B.Tech in Petroleum Engineering",
+    institute: "IIT(ISM) Dhanbad",
+  },
+  {
+    year: 2022,
+    designation: "Software Developer - Intern",
+    institute: "Iauro Systems Pvt. Ltd.",
   },
   {
     year: 2019,
-    designation: 'Higher Secondary Education',
-    institute: 'Arihant College of Arts, Commerce & Science, Pune (MH)',
+    designation: "Higher Secondary Education",
+    institute: "Arihant College of Arts, Commerce & Science, Pune (MH)",
   },
   {
     year: 2018,
-    designation: 'Eleventh Grade',
-    institute: 'Arihant College of Arts, Commerce & Science, Pune (MH)',
+    designation: "Eleventh Grade",
+    institute: "Arihant College of Arts, Commerce & Science, Pune (MH)",
   },
   {
     year: 2017,
-    designation: 'Senior Secondary Education',
-    institute: 'Central Public School, Mehkar (MH)',
+    designation: "Senior Secondary Education",
+    institute: "Central Public School, Mehkar (MH)",
   },
 ];
 
 export const heroSectionText =
-  'As a full-stack web developer, I possess a versatile skill set in both front-end and back-end development, ready to tackle diverse web projects with enthusiasm and a drive for excellence.';
+  "As a full-stack web developer, I possess a versatile skill set in both front-end and back-end development, ready to tackle diverse web projects with enthusiasm and a drive for excellence.";
 
 export const aboutMeText =
-  'As a full-stack web developer, I possess a versatile skill set in both front-end and back-end development, ready to tackle diverse web projects with enthusiasm and a drive for excellence.';
+  "As a full-stack web developer, I possess a versatile skill set in both front-end and back-end development, ready to tackle diverse web projects with enthusiasm and a drive for excellence.";
 
 export const contactDetails = {
-  phone: '+91-89755-17715',
-  email: ' yashagrawal01meh@gmail.com',
+  phone: "+91-89755-17715",
+  email: " yashagrawal01meh@gmail.com",
   address:
-    'Housr 84, Plot No.183, Masjid Banda Rd, Sai Pruthvi Enclave, Kondapur, Hyderabad, Telangana 500084',
+    "Housr 84, Plot No.183, Masjid Banda Rd, Sai Pruthvi Enclave, Kondapur, Hyderabad, Telangana 500084",
 };
+export const certificates = [
+  {
+    title: "JavaScript(Intermediate)",
+    issuedBy: "Hackerrank",
+    issuedOn: "Jan 2024",
+    logo: "./images/hackerrank.png",
+    link: "https://www.hackerrank.com/certificates/62b40426f498",
+  },
+  {
+    title: "JavaScript(Intermediate)",
+    issuedBy: "Hackerrank",
+    issuedOn: "Jan 2024",
+    logo: "./images/hackerrank.png",
+    link: "https://www.hackerrank.com/certificates/62b40426f498",
+  },
+  {
+    title: "JavaScript(Intermediate)",
+    issuedBy: "Udemy",
+    issuedOn: "Jan 2024",
+    logo: "./images/udemy.png",
+    link: "https://www.hackerrank.com/certificates/62b40426f498",
+  },
+  {
+    title: "JavaScript(Intermediate)",
+    issuedBy: "Udemy",
+    issuedOn: "Jan 2024",
+    logo: "./images/udemy.png",
+    link: "https://www.hackerrank.com/certificates/62b40426f498",
+  },
+];
