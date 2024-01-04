8c6f695d8bf55cbee6ff01839bba29efc5010f2d
Yash Agrawal <62216513+Yash01607@users.noreply.github.com>
2024-01-04 17:41:41 +0530

    Created Cerficicates.js

---
+++ src/components/Certificates/Certificates.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import {
+  CertificateCard,
+  CardInfo,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  TitleContent,
+  Img,
+} from "./certificateStyles";
+
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { certificates } from "../../constants/constants";
+
+const Certificates = () => (
+  <Section nopadding id={"projects"}>
+    <SectionDivider />
+    <SectionTitle main>Certificates</SectionTitle>
+    <GridContainer>
+      {certificates.map((c, i) => {
+        return (
+          <CertificateCard key={i}>
+            <Img src={c.logo} alt={c.title} title={`Visit ${c.title}`} />
+            <CardInfo className="card-info">
+              {" "}
+              <TitleContent>
+                <a href={c.link} target="_blank" rel="noreferrer">
+                  <HeaderThree>{c.title}</HeaderThree>
+                </a>
+              </TitleContent>
+              {c.issuedBy}
+              <br />
+              {c.issuedOn}
+            </CardInfo>
+          </CertificateCard>
+        );
+      })}
+    </GridContainer>
+  </Section>
+);
+
+export default Certificates;
