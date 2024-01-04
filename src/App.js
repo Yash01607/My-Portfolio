189e873416c267e8ee6d08f2cbd0882150a9db32
Yash Agrawal <62216513+Yash01607@users.noreply.github.com>
2024-01-04 17:44:20 +0530

    Added Certificates to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Hero from './components/Hero/Hero';
 import Projects from './components/Projects/Projects';
 import Technologies from './components/Technologies/Technologies';
 import About from './components/About/About';
+import Certificates from "./components/Certificates/Certificates";
+
 import { Layout } from './layout/Layout';
 import { Section } from './styles/GlobalComponents';
 import './App.css';
@@ -16,6 +18,7 @@ function App() {
       </Section>
       <Projects />
       <Technologies />
+      <Certificates/>
       <About />
     </Layout>
   );
